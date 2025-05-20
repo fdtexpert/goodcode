06e06edff7f3d075e55a6ff22ed1ad3e681d1c0c
fdtexpert <34168186+fdtexpert@users.noreply.github.com>
2025-05-20 22:24:59 +0500

    Update n8n_install.sh

--- n8n_install.sh
+++ n8n_install.sh
@@ -7,7 +7,7 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install -y docker-ce
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 echo "✅ Docker installation completed!"
 
 # Creating n8n Data Volume
